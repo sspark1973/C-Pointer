e9dd63b2ec861df00ba5239a73cfb804c451e42f
sspark1973 <sspark1973@gmail.com>
2014-07-14 18:50:29 -0700

    Create 01.c

---
+++ 01.c
@@ -0,0 +1,18 @@
+/* 
+printf(%#010x\n, imsip);
+#은 주소를 표현하기 위하여 많이 사용하는 형식
+
+#10x : 847598 -> 0x847598
+#010x : 8475 -> 0x00008475 - 10자리 0x포함
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int *imsip;
+  
+  printf("%p\n", imsip);
+  printf("%p\n", &imsip);
+}
